9fedf7eca9c8f6e80e0c3c6b29b5149a0e8bfadf
smorgannicole <morgann.smithh@yahoo.com>
2024-05-17 16:02:48 -0700

    forgot to add handle scroll

--- src/components/FinishedProjects.js
+++ src/components/FinishedProjects.js
@@ -6,6 +6,9 @@ import catTinder from "../assets/cat-tinder.png"
 import weatherMap from "../assets/map1.png"
 
 const FinishedProjects = () => {
+  const handleViewProj = () => {
+    window.scrollTo(0, 0)
+  }
   return (
     <>
       <div className="flex py-10 gap-14">
@@ -13,6 +16,7 @@ const FinishedProjects = () => {
           to="/show1"
           className="w-7/12 h-101 relative overflow-hidden bg-pink-750 bg-opacity-55 rounded-4xl py-7 px-9 hover:bg-opacity-65 hover:cursor-pointer hover:drop-shadow-2xl hover:-translate-y-1 transition-all duration-500 group"
           tabIndex="0"
+          onClick={handleViewProj}
         >
           <div className="flex flex-col text-gray-950">
             <h3 className="font-title text-5xl flex justify-end">
