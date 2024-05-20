202bf3de71faf1fb15602fffd1271048c7e52939
smorgannicole <morgann.smithh@yahoo.com>
2024-05-20 12:39:27 -0700

    styled skip nav

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -33,38 +33,34 @@ const NavBar = ({ projTitle }) => {
             isScrolled ? "bg-white py-5 px-4 rounded-full" : null
           }`}
         >
-          {!isScrolled ? (
-            <li className="transition-all duration-300 flex align-top">
-              <a
-                href="#skip-nav"
-                className="transition-all duration-300 whitespace-no-wrap flex items-center relative sr-only focus:not-sr-only focus:bg-white focus:text-black focus:p-2"
-                onMouseEnter={() => setSkipHovered(true)}
-                onMouseLeave={() => setSkipHovered(false)}
-                onFocus={() => setSkipHovered(true)}
-                onBlur={() => setSkipHovered(false)}
+          <li className="transition-all duration-300">
+            <a
+              href="#skip-nav"
+              className="transition-all focus:px-3 rounded-full duration-300 whitespace-no-wrap flex items-center relative sr-only focus:not-sr-only focus:bg-blue-100 focus:text-black"
+              onMouseEnter={() => setSkipHovered(true)}
+              onMouseLeave={() => setSkipHovered(false)}
+              onFocus={() => setSkipHovered(true)}
+              onBlur={() => setSkipHovered(false)}
+            >
+              Skip Navigation
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className={`w-6 h-6 ml-2 absolute -right-7 transition-opacity ${
+                  skipHovered ? "opacity-100" : "opacity-0"
+                }`}
               >
-                <span>Skip Navigation</span>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth={1.5}
-                  stroke="currentColor"
-                  className={`w-6 h-6 ml-2 absolute -right-7 transition-opacity ${
-                    skipHovered ? "opacity-100" : "opacity-0"
-                  }`}
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="m9 12.75 3 3m0 0 3-3m-3 3v-7.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
-                  />
-                </svg>
-              </a>
-            </li>
-          ) : (
-            ""
-          )}
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="m9 12.75 3 3m0 0 3-3m-3 3v-7.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+                />
+              </svg>
+            </a>
+          </li>
           <li>
             <NavLink
               to="/"
