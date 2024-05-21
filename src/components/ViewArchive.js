887eaa461f7a40ed1a3b03e56c41db955bbae9a2
smorgannicole <morgann.smithh@yahoo.com>
2024-05-21 15:18:08 -0700

    reconfigured some code to clean clutter

---
+++ src/components/ViewArchive.js
@@ -0,0 +1,32 @@
+import React from "react"
+import { Link } from "react-router-dom"
+
+const ViewArchive = ({ handleViewArchive }) => {
+  return (
+    <div className="font-text text-xl ml-3 mt-4 whitespace-no-wrap flex items-center gap-1 text-gray-950">
+      <Link
+        to="/archive"
+        className="hover:underline underline-offset-2"
+        onClick={handleViewArchive}
+      >
+        View Full Project Archive
+      </Link>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth={1.5}
+        stroke="currentColor"
+        className="w-6 h-6"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="m12.75 15 3-3m0 0-3-3m3 3h-7.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+        />
+      </svg>
+    </div>
+  )
+}
+
+export default ViewArchive
--- src/pages/Work.js
+++ src/pages/Work.js
@@ -1,10 +1,20 @@
-import React from "react"
+import React, { useState } from "react"
 import AmbientBG from "../components/AmbientBG"
 import NavBar from "../components/NavBar"
 import Hero from "../components/Hero"
-import Projects from "../components/Projects"
+import FinishedProjects from "../components/FinishedProjects"
+import InProgressProjects from "../components/InProgressProjects"
+import Footer from "../components/Footer"
+import Footnote from "../components/Footnote"
+import ViewArchive from "../components/ViewArchive"
 
 const Work = () => {
+  const [displayFinished, setDisplayFinished] = useState(true)
+
+  const handleViewArchive = () => {
+    window.scrollTo(0, 0)
+  }
+
   return (
     <div className="bg-blue-100 min-h-screen bg-opacity-50 px-24 py-10 overflow-hidden">
       <NavBar />
@@ -14,7 +24,21 @@ const Work = () => {
       <div className="flex justify-end 2xl:justify-center pt-44 pb-56">
         <Hero />
       </div>
-      <Projects />
+      <main className="relative">
+        {displayFinished ? (
+          <FinishedProjects
+            displayFinished={displayFinished}
+            setDisplayFinished={setDisplayFinished}
+          />
+        ) : (
+          <InProgressProjects
+            displayFinished={displayFinished}
+            setDisplayFinished={setDisplayFinished}
+          />
+        )}
+        <ViewArchive handleViewArchive={handleViewArchive} />
+        <Footer footnote={displayFinished ? <Footnote /> : null} />
+      </main>
     </div>
   )
 }
