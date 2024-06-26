632dc90e09ad5b12c25273891fad1d970cf84cd9
Morgan <smorgannicole@gmail.com>
2024-06-26 10:30:13 -0500

    refactored to minimize repetition

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
-import React from "react"
-import "./App.css"
-import { Route, Routes } from "react-router-dom"
-import Work from "./pages/Work"
-import About from "./pages/About"
-import Table from "./pages/Table"
-import Show1 from "./pages/Show1"
-import Show2 from "./pages/Show2"
-import Show3 from "./pages/Show3"
-import Show4 from "./pages/Show4"
-import Contact from "./pages/Contact"
+import React from "react";
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Work from "./pages/Work";
+import About from "./pages/About";
+import Table from "./pages/Table";
+import Show from "./pages/Show";
+import Contact from "./pages/Contact";
 
 const App = () => {
   const projects = [
@@ -30,6 +27,7 @@ const App = () => {
         "A React and Rails API project with a PostgreSQL database. Users can create events with descriptions, dates, and locations. Invitations can be sent to friends and family while financial goals for events can be set and tracked. User authentication and security are managed with Devise and JWT. Logged-in users can manage their events by adding, editing, or deleting them, ensuring full control and transparency. Ryan Lemus and Luis Moreno contributed to this project.",
     },
     {
+      id: 1,
       date: "04/2024",
       project: "Vibe Villas",
       builtWith: ["React", "Rails", "Reactstrap"],
@@ -38,6 +36,20 @@ const App = () => {
       ],
       description:
         "A React and Rails API project with a PostgreSQL database. User authentication and authorization are handled via Devise and JWT, enabling users to list units for rent and manage their listings. Visitors can view available units, access unit info, and engage in listing management functionalities upon logging in. Mark Smith contributed to this project.",
+      palette: [
+        "#DFAD9D",
+        "#F2C29D",
+        "#775455",
+        "#B4795A",
+        "#874C2C",
+        "#4C3C38",
+        "#957765",
+        "#FFFFFF",
+        "#F4D1A9",
+        "#E5C6A8",
+      ],
+      contributors: ["Mark Smith"],
+      contributorLinks: ["https://github.com/mark19242"],
     },
     {
       date: "03/2024",
@@ -65,6 +77,7 @@ const App = () => {
         "A React app where users can add and delete items from their carts. Interactive modals showcase food items illustrated using Adobe Fresco.",
     },
     {
+      id: 3,
       date: "03/2024",
       project: "Cat Tinder",
       builtWith: ["React", "Rails", "Reactstrap"],
@@ -73,6 +86,23 @@ const App = () => {
       ],
       description:
         "A full-stack, decoupled application with a PostgreSQL database. Users can view, heart, edit, add , and delete kitties. Kitties may be filtered by age and searched by name. Amir Jackson and Ryan Lemus contributed to this project.",
+      palette: [
+        "#212124",
+        "#1C1E1F",
+        "#333132",
+        "#5F5957",
+        "#232424",
+        "#1D1E20",
+        "#AD922A",
+        "#1B1D1E",
+        "#E8D32F",
+        "#22201F",
+      ],
+      contributors: ["Ryan Lemus", "Amir Jackson"],
+      contributorLinks: [
+        "https://github.com/Rlemus93",
+        "https://github.com/Rashadjaxon",
+      ],
     },
     {
       date: "03/2024",
@@ -121,12 +151,25 @@ const App = () => {
         "A React app where each roll corresponds to graphics and colors on a particular die. Users may switch between different themed dice and roll a new set of six corresponding emojis. Rolls are logged may be cleared at any time.",
     },
     {
+      id: 2,
       date: "12/2023",
       project: "Tea House",
       builtWith: ["React", "Bootstrap", "Boonaki Tea API"],
       link: ["https://github.com/smorgannicole/tea-react-project"],
       description:
         "A React app where visitors access tea info via the Boonaki API. Tea enthusiasts can add, delete, and edit teas, all stored locally.",
+      palette: [
+        "#AFB18D",
+        "#D2CFB7",
+        "#F0EEE9",
+        "#F4F5F7",
+        "#969C6E",
+        "#69807D",
+        "#F5F0DA",
+        "#D9C591",
+        "#A66E57",
+        "#869272",
+      ],
     },
     {
       date: "11/2023",
@@ -136,6 +179,7 @@ const App = () => {
       description: `An application where users can search coffees by name or filter by roast. Coffee cards may be clicked and "flipped" to reveal more info. The app enables users to create, edit, and delete their own custom coffees, stored locally for convenience. Nathan Pennington contributed to this project.`,
     },
     {
+      id: 4,
       date: "11/2023",
       project: "Weather Map",
       builtWith: ["Javascript", "Bootstrap", "OpenWeather API", "Mapbox API"],
@@ -143,7 +187,19 @@ const App = () => {
         "https://github.com/smorgannicole/codeup-web-exercises/blob/main/js/weather_map.js",
       ],
       description:
-        "Built using HTML, CSS, and JavaScript. OpenWeatherMap API is used to display weather info in a specific location. Users can search for desired locations with the forecast updated dynamically. Mapbox API is used to visually transition from the previous location to the newly searched one.",
+        "Built using HTML, CSS, and JavaScript. OpenWeather API is used to display weather info in a specific location. Users can search for desired locations with the forecast updated dynamically. Mapbox API is used to visually transition from the previous location to the newly searched one.",
+      palette: [
+        "#CFD0E0",
+        "#FEFEFE",
+        "#4E0295",
+        "#6EBAF6",
+        "#530C9A",
+        "#B7DDFB",
+        "#B798D4",
+        "#B7AFCC",
+        "#AFCBE8",
+        "#672CA4",
+      ],
     },
     {
       date: "11/2023",
@@ -165,19 +221,27 @@ const App = () => {
       description:
         "Designed as a platform for honing proficiency in media queries, this project presents three wireframes of layouts to be replicated. The objective is to rebuild the site from scratch with detail and precision.",
     },
-  ]
+  ];
+
+  const showProjects = projects.filter((project) =>
+    [1, 2, 3, 4].includes(project.id)
+  );
+
   return (
     <Routes>
       <Route path="/" element={<Work />} />
       <Route path="/archive" element={<Table projects={projects} />} />
       <Route path="/about" element={<About />} />
-      <Route path="/show1" element={<Show1 />} />
-      <Route path="/show2" element={<Show2 />} />
-      <Route path="/show3" element={<Show3 />} />
-      <Route path="/show4" element={<Show4 />} />
+      {showProjects.map((project) => (
+        <Route
+          key={project.id}
+          path={`/show${project.id}`}
+          element={<Show project={project} />}
+        />
+      ))}
       <Route path="/contact" element={<Contact />} />
     </Routes>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ src/pages/Show.js
@@ -0,0 +1,195 @@
+import React from "react";
+import NavBar from "../components/NavBar";
+import Footer from "../components/Footer";
+import vibeVillas from "../assets/vibe-villas.png";
+import vv1 from "../assets/vv1.png";
+import vv3 from "../assets/vv3.png";
+import vv4 from "../assets/vv4.png";
+import vv5 from "../assets/vv5.png";
+import tea from "../assets/tea.png";
+import t1 from "../assets/t1.png";
+import t2 from "../assets/t2.png";
+import t3 from "../assets/t3.png";
+import catTinder from "../assets/cat-tinder.png";
+import tinder1 from "../assets/tinder1.png";
+import tinder2 from "../assets/tinder2.png";
+import tinder3 from "../assets/tinder3.png";
+import tinder4 from "../assets/tinder4.png";
+import tinder5 from "../assets/tinder5.png";
+import map1 from "../assets/map1.png";
+import map2 from "../assets/map2.png";
+import map3 from "../assets/map3.png";
+import map4 from "../assets/map4.png";
+
+const Show = ({ project }) => {
+  const {
+    date,
+    project: projectName,
+    builtWith,
+    link,
+    description,
+    palette,
+    contributors,
+    contributorLinks,
+  } = project;
+
+  let projectImages;
+  switch (projectName) {
+    case "Vibe Villas":
+      projectImages = [vibeVillas, vv1, vv3, vv4, vv5];
+      break;
+    case "Tea House":
+      projectImages = [tea, t1, t2, t3];
+      break;
+    case "Cat Tinder":
+      projectImages = [catTinder, tinder1, tinder2, tinder3, tinder4, tinder5];
+      break;
+    case "Weather Map":
+      projectImages = [map1, map2, map3, map4];
+      break;
+    default:
+      projectImages = null;
+      break;
+  }
+
+  return (
+    <div
+      className="font-text text-gray-950 min-h-screen bg-opacity-50 bg-blue-100 py-6 px-5 screen-md-sm:py-12 screen-md-sm:px-10 flex flex-col"
+      style={{ alignItems: "center" }}
+    >
+      <div className="w-full max-w-screen-2xl">
+        <div className="pt-20 pl-5">
+          <NavBar projTitle={projectName} />
+        </div>
+        <div className="flex flex-col" style={{ alignItems: "center" }}>
+          <h1 className="font-title text-3xl screen-md-sm:text-5xl screen-md:text-6xl pt-20 screen-md:pt-8 items-center flex flex-col justify-center relative w-fit text-center">
+            {projectName}
+            <div className="flex justify-center absolute -bottom-6 left-0 right-0">
+              {palette.map((color, index) => (
+                <div
+                  key={index}
+                  style={{
+                    backgroundColor: color,
+                    width: `${100 / palette.length}%`,
+                  }}
+                  className={`w-11 h-4 ${
+                    index === 0 ? "rounded-tl-lg rounded-bl-lg" : ""
+                  } ${
+                    index === palette.length - 1
+                      ? "rounded-tr-lg rounded-br-lg"
+                      : ""
+                  }`}
+                ></div>
+              ))}
+            </div>
+          </h1>
+        </div>
+        <div className="flex justify-center gap-8 screen-md-sm-660:gap-24 mt-10 screen-md-sm:mt-20 text-sm screen-md-sm-660:text-xl">
+          {contributors && (
+            <div className="flex flex-col">
+              <p className="font-bold">Contributors</p>
+              <ul>
+                {contributors.map((contributor, index) => (
+                  <li key={index} className="mt-2">
+                    <a
+                      href={contributorLinks[index]}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="underline flex items-center transition-all duration-300 hover:opacity-70"
+                    >
+                      <span className="flex items-center">
+                        {contributor}
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          viewBox="0 0 20 20"
+                          fill="currentColor"
+                          className="w-6 h-6 ml-1"
+                        >
+                          <path
+                            fillRule="evenodd"
+                            d="M5.22 14.78a.75.75 0 0 0 1.06 0l7.22-7.22v5.69a.75.75 0 0 0 1.5 0v-7.5a.75.75 0 0 0-.75-.75h-7.5a.75.75 0 0 0 0 1.5h5.69l-7.22 7.22a.75.75 0 0 0 0 1.06Z"
+                            clipRule="evenodd"
+                          />
+                        </svg>
+                      </span>
+                    </a>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          )}
+          <div className="flex flex-col">
+            <p className="font-bold">Date of Completion</p>
+            <ul>
+              <li className="mt-2">
+                <p>{date}</p>
+              </li>
+            </ul>
+          </div>
+          <div className="flex flex-col">
+            <p className="font-bold">Built With</p>
+            <ul>
+              {builtWith.map((tech, index) => (
+                <li key={index} className="mt-2">
+                  {tech}
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div className="flex flex-col">
+            <p className="font-bold">View Repo</p>
+            <ul>
+              {link.map((repo, index) => (
+                <li key={index} className="mt-2">
+                  <a href={repo} target="_blank" rel="noopener noreferrer">
+                    <span className="underline flex flex-wrap transition-all duration-300 hover:opacity-70">
+                      GitHub
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 20 20"
+                        fill="currentColor"
+                        className="w-6 h-6"
+                      >
+                        <path
+                          fillRule="evenodd"
+                          d="M5.22 14.78a.75.75 0 0 0 1.06 0l7.22-7.22v5.69a.75.75 0 0 0 1.5 0v-7.5a.75.75 0 0 0-.75-.75h-7.5a.75.75 0 0 0 0 1.5h5.69l-7.22 7.22a.75.75 0 0 0 0 1.06Z"
+                          clipRule="evenodd"
+                        />
+                      </svg>
+                    </span>
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+        <h2 className="font-title text-2xl screen-md-sm:text-4xl screen-md:text-5xl mt-10 screen-md-sm:mt-20 items-center flex flex-col justify-center">
+          Overview
+        </h2>
+        <article className="mt-5 screen-md-sm:mt-10 flex justify-center">
+          <p className="text-sm screen-md-sm-660:text-xl w-120">
+            {description}
+          </p>
+        </article>
+        {projectImages && (
+          <div>
+            {projectImages.map((image, index) => (
+              <div className="flex justify-center" key={index}>
+                <img
+                  className="max-h-111 rounded-lg mt-10 screen-md-sm:mt-20"
+                  src={image}
+                  alt={`Project image ${index + 1} from ${projectName}`}
+                />
+              </div>
+            ))}
+          </div>
+        )}
+        <div className="w-full max-w-screen-md-lg mx-auto">
+          <Footer />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Show;
